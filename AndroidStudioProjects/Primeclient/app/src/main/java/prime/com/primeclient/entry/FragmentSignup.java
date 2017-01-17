0b795b60d3080a4883bd71e04f110fa81b3dac62
BrahmaRishi <rishi.brahma93@gmail.com>
2017-01-17 21:36:30 +0530

    signup 2

--- AndroidStudioProjects/Primeclient/app/src/main/java/prime/com/primeclient/entry/FragmentSignup.java
+++ AndroidStudioProjects/Primeclient/app/src/main/java/prime/com/primeclient/entry/FragmentSignup.java
@@ -3,16 +3,24 @@ package prime.com.primeclient.entry;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import prime.com.primeclient.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FragmentSignup extends Fragment {
+public class FragmentSignup extends Fragment implements View.OnClickListener {
+
+    View _rootView;
+    EditText input_name, input_email, input_password;
+    Button button_signup;
 
 
     public FragmentSignup() {
@@ -24,7 +32,25 @@ public class FragmentSignup extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_signup, container, false);
+        if (_rootView == null) {
+            _rootView = inflater.inflate(R.layout.fragment_signup, container, false);
+        }
+        return _rootView;
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        input_name = (EditText) getActivity().findViewById(R.id.input_name);
+        input_email = (EditText) getActivity().findViewById(R.id.input_email);
+        input_password = (EditText) getActivity().findViewById(R.id.input_password);
+        button_signup = (Button) getActivity().findViewById(R.id.button_signup);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button_signup) {
+            Toast.makeText(getActivity(), input_name.getText().toString(), Toast.LENGTH_LONG).show();
+        }
+    }
 }
